7bda264257627fd63487828aadccae9d96d416e8
carles <carles@carles>
2018-02-07 17:44:10 +0100

    new commit

--- src/Activitats/Activitat1.java
+++ src/Activitats/Activitat1.java
@@ -16,7 +16,8 @@ public class Activitat1 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        System.out.println("Hola amic");
+        System.out.println("Hola amic que tal");
+        System.out.println("actualitzat");
     }
     
 }
